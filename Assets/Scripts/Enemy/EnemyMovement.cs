338c1cecc66598addbee1bab442bfdea4d1ab36d
BigBoss435 <deividas.kalvelis@gmail.com>
2023-11-05 13:09:37 +0200

    Fixed terrain generation bug, made enemies slide a bit

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemyMovement : MonoBehaviour
 {
     EnemyStats enemy;
     Transform player;
+    Rigidbody2D rb;
 
     void Start()
     {
         enemy = GetComponent<EnemyStats>();
         player = FindObjectOfType<PlayerStats>().transform;
+        rb = GetComponent<Rigidbody2D>();
+        Collider2D col = rb.GetComponent<Collider2D>();
+
+        PhysicsMaterial2D physMat = new PhysicsMaterial2D();
+        physMat.friction = 0.05f;
+
+        col.sharedMaterial = physMat;
     }
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemy.currentMoveSpeed * Time.deltaTime);
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.velocity = direction * enemy.currentMoveSpeed;
     }
 }
