c2744b599c54799792d220f28a9f9cde6d04814b
Deividas Kalvelis <deividas.kalvelis@gmail.com>
2023-11-26 14:41:03 +0200

    Fixed pulling, decreased xp gain

--- Assets/Scripts/Player/PlayerCollector.cs
+++ Assets/Scripts/Player/PlayerCollector.cs
@@ -31,4 +31,14 @@ public class PlayerCollector : MonoBehaviour
             collectible.Collect();
         }
     }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.TryGetComponent(out ICollectible collectible))
+        {
+            Rigidbody2D rb = col.gameObject.GetComponent<Rigidbody2D>();
+            Vector2 forceDirection = (transform.position - col.transform.position).normalized;
+            rb.AddForce(forceDirection * pullSpeed);
+        }
+    }
 }
